8950b322c3c56861809490120194acdaf74c470d
pbartkowiak-dev <bartkop@wz.hasbro.com>
2024-08-31 16:28:46 +0200

    set derived values in results bar component

--- src/app/components/results-bar/results-bar.component.ts
+++ src/app/components/results-bar/results-bar.component.ts
@@ -14,30 +14,64 @@ export class ResultsBarComponent implements OnInit {
   lowest: number = 0;
   highest: number = 0;
   summary: string = '';
-  results: RollResult[] = []
 
-  constructor(
-    private rollService: RollService
-  ) { }
+  constructor(private rollService: RollService) {
+    this.rollService.getResults.subscribe(results => {
+      if (results.length) {
+        const resultsValues = results.map((result) => {
+          return result.result
+        });
+
+        this.total = resultsValues.reduce((previousValue, currentValue)=> {
+          return previousValue + currentValue;
+        }, 0);
+
+        this.summary = this.getSummary(results);
+
+        if (results.length >= 2) {
+          this.highest = Math.max(...resultsValues);
+          this.lowest = Math.min(...resultsValues);
+        }
+      } else {
+        this.total = 0;
+        this.lowest = 0;
+        this.highest = 0;
+        this.summary = '';
+      }
+    })
+  }
+
+  getSummary(results: RollResult[]): string {
+    const diceCount = results.reduce((previousValue, { faces }) => {
+      const newValue = {...previousValue};
+      const key = `d${faces}`;
+      if (previousValue.hasOwnProperty(key)) {
+        newValue[key] += 1
+      } else {
+        newValue[key] = 1;
+      }
+      return newValue;
+    }, {} as { [key: string]: number });
+
+    const entriesArray = Object.entries(diceCount).sort((a, b) => {
+      const aNumber = parseInt(a[0].substring(1));
+      const bNumber = parseInt(b[0].substring(1));
+      return aNumber - bNumber;
+    });
+
+    return entriesArray.reduce((previousValue, currentValue) => {
+       const dice = currentValue[0];
+       const amount = currentValue[1];
+
+       if (previousValue === '') {
+         return previousValue + amount + dice;
+       } else {
+         return previousValue + ' + ' + amount + dice;
+       }
+
+     }, '')
+  }
 
   ngOnInit() {
-    this.results = this.rollService.getResults();
-
-    console.log('results bar results', this.results)
-
-    if (this.results.length) {
-      const resultsValues = this.results.map((result) => {
-        return result.result
-      });
-
-      this.total = resultsValues.reduce((previousValue, currentValue)=> {
-        return previousValue + currentValue;
-      }, 0);
-
-      this.highest = Math.max(...resultsValues);
-      this.lowest = Math.max(...resultsValues);
-    }
-
-
   }
 }
