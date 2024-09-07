70b029468cc814a694ec8317670ff9d17a0b75b9
pbartkowiak-dev <bartkop@wz.hasbro.com>
2024-09-07 09:31:55 +0200

    handle invalid modifier values

--- src/app/components/modifier-input/modifier-input.component.ts
+++ src/app/components/modifier-input/modifier-input.component.ts
@@ -1,21 +1,32 @@
 import { Component } from '@angular/core';
 import { ModifierService } from '../../services/modifier.service';
-import { NgIf } from '@angular/common';
+import { NgClass, NgIf } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-modifier-input',
   standalone: true,
-  imports: [NgIf, FormsModule],
+  imports: [NgIf, FormsModule, NgClass],
   templateUrl: './modifier-input.component.html',
   styleUrl: './modifier-input.component.css',
 })
 export class ModifierInputComponent {
-  modifier: string = '0';
+  modifierLocal: string = '0';
+  isInvalid: boolean = false;
   constructor(private modifierService: ModifierService) {
     modifierService.getModifier.subscribe((newValue) => {
-      this.modifier = `${newValue > 0 ? '+' + newValue : newValue}`;
+      this.modifierLocal = `${newValue > 0 ? '+' + newValue : newValue}`;
+      this.isInvalid = false;
     });
+  }
+
+  onInputChange(value: string) {
+    const parsedValue = parseInt(value, 10);
+    if (isNaN(parsedValue)) {
+      this.isInvalid = true;
+      return;
+    }
+    this.modifierService.setModifier(parsedValue);
   }
 
   handleIncrease() {
